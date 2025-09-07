0aa686038614f3c5ff3df1f9b977a705b051d0da
Uramix <158606954+Uramix2@users.noreply.github.com>
2025-09-08 00:18:08 +0200

    Update poker_ilan.py

--- poker_ilan.py
+++ poker_ilan.py
@@ -1,1 +1,6 @@
-dqz
+# my bad j'ai écrasé ton fichier avec le mien j'ai juste fais un fonction et corriger des pb 
+# il va falloir revoir les class ils sont parfois pas opti du tout genre des paramètres devraient peut etre aller dans d'autres ect
+# la fonction hand_rank elle casse les couilles tout simplement j'ai pas eu trop de le tps de regarder en profondeur mais il faudrait la simplifier 
+# pour généré un round j'ai peu réfléchis et il faudrait juste check si les blindes sont égales avec tout les joureurs
+#incrementer un moyen pour changer les blinds après chaque main (ça va pas trop chiant)
+# ne pas oublier de rénialiser les variable après utilisation  genre le pot meme si c'est deja fait il y en a peut d'autre 

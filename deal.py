beba4c2365b5b6a77884e09193356fa7f5dde104
John The Reaper <ilan.dujols@outlook.fr>
2025-10-10 12:34:11 +0200

    Refactor Deal class for card initialization
    Refactor card initialization and update card values and colors.

--- deal.py
+++ deal.py
@@ -1,33 +1,28 @@
 from random import shuffle
 
-class Deal:
-    valeurs = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valet", "Dame", "Roi"]
-    couleurs = ["Coeur", "Careau", "Trefle", "Pique"]
-
+class Deal:    
     def __init__(self):
-        self.paquet = self.creer_paquet()
+        self.cards = self.cards_init()
+        self.board = []
+        self.colors = ['D', 'H', 'S', 'C']  # Diamond: carreau , Heart: coeur, Spade: pique, Club: trèfle
+        self.values = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+    
+    def cards_init(self):
+        """
+        Initialisation du paquet de carte 
+        """
+        self.cards = [(color, value) for color in colors for value in values]
+        shuffle(self.cards)
 
-    def creer_paquet(cls):
-        paquet = []
-        for couleur in cls.couleurs:
-            for valeur in cls.valeurs:
-                paquet.append([valeur, couleur])
-        return paquet
-
-    def melanger(self):
-        from random import shuffle
-        shuffle(self.paquet)
-
-    def distribuer(self, n:str):
+    def distribuer(self):
+        """
+        Distribue les mains aléatoirement aux joueurs via les class et les retire du jeu
+        -- # -- FONCTION A FAIRE UNE FOIS QUE LES CLASS JOUEURS SONT INITIALISES -- # -- 
+        """
         pass
 
-    def board(self):
+    def board(self, ):
+        """
+        Gère les différentes étapes de la partie qui concerne le board : flop, turn, river
+        """
         pass
-
-    def 
-
-
-
-
-
-

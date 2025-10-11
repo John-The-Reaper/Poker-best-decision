661ac44ca63712789ee7e7160bcaec3f630d2f0e
John The Reaper <ilan.dujols@outlook.fr>
2025-10-11 12:54:45 +0200

    Refactor GraphicalRendering and enhance docstring
    Updated class names and improved docstring for player data processing.

---
+++ GraphicalRendering.py
@@ -0,0 +1,35 @@
+from mathplotlib import pyplot as plt
+from Stats.py import Stats
+
+class GraphicalRedering:
+  
+  def __init__(self):
+    self.simulation = Stats()
+    self.data = simulation.game()
+    pass
+
+  def read_json(path:str):
+    """
+    Récupération des données présente grâce à la class Stats (ou via des la lecture des fichiers JSON
+    """
+    pass
+    
+  def players_data():
+    """
+    Traitement de donnée individuel des joueurs et sauvegarde dans un fichier JSON
+    Returns : average_bet, win_rate, average_loss, average_win, 
+    """
+    pass
+    
+  def stack_evolution()
+    """
+    Enregistre le graphique de l'évolution des staks des joueurs
+    """
+    pass
+
+
+
+
+
+
+

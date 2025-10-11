721bade38a26ed3281bb18d92f7c4386e0ce6553
John The Reaper <ilan.dujols@outlook.fr>
2025-10-11 13:03:53 +0200

    Create Game.py

---
+++ Game.py
@@ -0,0 +1,25 @@
+"""
+
+import de toute les class des joueurs
+
+"""
+
+class Game:
+  def __init__(self):
+    """
+    Initalisation de toute les classes joueurs
+    """
+    pass
+
+######### Pas certain qu'on est besoin de cette fonciton #########
+  def get_player_action(self, player, data):
+    """
+    Prends en paramètre une class d'un joueur et les données de la partie pour pouvoir r&écupérer la position 
+    """
+    pass
+
+  def game(self)
+    """
+    Génère le déroulement complte de la partie et renvoie un dataframe des données de la partie (il sera donné à la class stats)
+    """
+    pass

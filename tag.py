9d603dc0e93c6bb9dfe939f6fdd97a37096b3b08
Pr1me000 <overall-rug-dust@duck.com>
2025-10-28 10:35:02 +0000

    Implement Tag class for poker strategy

---
+++ tag.py
@@ -0,0 +1,33 @@
+import numpy as np
+# from stats.py import stat
+
+class Tag():
+    def __init__(self, win_chance, position, stack, amount_to_call): 
+        self.win_chance = win_chance
+        self.position = position
+        self.stack = stack
+        self.amount_to_call = amount_to_call
+
+        #Parameters
+        self.min_bet = 0.1
+        self.max_bet = 0.9
+        self.behavior_level = 0.55
+        self.aggressiveness = 12.0
+
+    def stack_percent(self, win_chance): #calcul le pourcentage de la stack que le joueur veux miser
+        exponent_input = -self.aggressiveness * (win_chance - self.behavior_level)
+        result = self.min_bet + (self.max_bet - self.min_bet) / (1 + np.exp(exponent_input))
+        return round(result,2) #renvoie 2 chiffres après la virgule
+    
+    def action(self):
+        if self.amount_to_call <= self.stack:  # Fallow dès qu'il peut
+            return "Fallow"
+        elif self.amount_to_call > self.stack: # fold si pas assez pour Fallow
+            return "Fold"
+
+
+    stack_percent(win_chance)
+
+
+objet = Tag(win_chance=0.5, position = False, stack=100)
+print(objet.stack_percent(objet.win_chance))

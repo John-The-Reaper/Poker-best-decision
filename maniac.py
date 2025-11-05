50d942144b445eb17698baddef05cd8ab5e83d98
Pr1me000 <overall-rug-dust@duck.com>
2025-11-05 15:24:06 +0000

    Add Maniac class for poker bot behavior
    Implement the Maniac class with betting logic and stack management.

---
+++ maniac.py
@@ -0,0 +1,42 @@
+import numpy as np # <-- Correction 1 : Import de numpy
+# from stats.py import stat
+
+class Maniac():
+    def __init__(self, position, stack): 
+        self.win_chance = 0.50
+        self.position = position
+        self.stack = stack
+
+        #Parameters
+        self.min_bet = 0.2
+        self.max_bet = 1
+        self.behavior_level = 0.40
+        self.aggressiveness = 15.0
+
+    def stack_percent(self, win_chance, board, state): #calcul le pourcentage de la stack que le joueur veux miser
+        exponent_input = -self.aggressiveness * (win_chance - self.behavior_level)
+        result = self.min_bet + (self.max_bet - self.min_bet) / (1 + np.exp(exponent_input))
+        return round(result,2) #renvoie 2 chiffres après la virgule
+    
+    def action(self, amount_to_call, board, state):
+        bot_bet = self.stack_percent(self.win_chance) * self.stack
+
+        if amount_to_call == 0 and self.stack_percent(self.win_chance) < 0.1:
+            return {"check": True}
+        
+        elif amount_to_call <= bot_bet >= amount_to_call * 1.1:
+            return {"call": amount_to_call}
+
+        elif amount_to_call < bot_bet:
+            return {"bet": round(bot_bet,0)}
+
+        elif amount_to_call > bot_bet:
+            return {"fold": True}
+        
+        
+
+
+
+
+#objet = Maniac(win_chance=0.5, position = False, stack=100)
+#print(objet.stack_percent(objet.win_chance))

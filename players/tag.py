535919f46a28ff2b511ef5d51bd40fb87cbc80f4
Pr1me000 <overall-rug-dust@duck.com>
2025-11-08 19:55:25 +0000

    Refactor Tag class methods and parameters

--- players/tag.py
+++ players/tag.py
@@ -1,11 +1,9 @@
-import numpy as np # <-- Correction 1 : Import de numpy
-# from stats.py import stat
+import numpy as np
 
 class Tag():
-    def __init__(self, position, stack): 
-        self.win_chance = 0.50
-        self.position = position
+    def __init__(self, stack): 
         self.stack = stack
+        self.win_chance = 0.6
 
         #Parameters
         self.min_bet = 0.10
@@ -13,44 +11,43 @@ class Tag():
         self.behavior_level = 0.55
         self.aggressiveness = 12.0
 
-    def stack_percent(self, win_chance, board, state): #calcul le pourcentage de la stack que le joueur veux miser
+    def multiplicator(self, win_chance): #calcul le pourcentage de la stack que le joueur veux miser
         exponent_input = -self.aggressiveness * (win_chance - self.behavior_level)
-        result = self.min_bet + (self.max_bet - self.min_bet) / (1 + np.exp(exponent_input))
+        result = self.multiplicator_min + (self.multiplicator_max - self.multiplicator_min) / (1 + np.exp(exponent_input))
         return round(result,2) #renvoie 2 chiffres après la virgule
+
     
-    def action(self, amount_to_call, position):
-        if position == "utg":
-             position = 1.15
-        elif position == "hijack":
-             position = 1.18
-        elif position == "button":
-            position = 1.45
-        elif position == "small_blind":
-            position = 1.35
-        elif position == "big_blind":
-            position = 1.00
+    def action(self, amount_to_call, position, optimal_bet_amount, optimal_choice):
+        style_factor = self.multiplicator(self.win_chance) 
+        #Avantage position impactant style_factor :
+        if position == "button":
+            style_factor *= 1.15  # Très agressif en position (+ 15%)
         elif position == "cutt_off":
-            position = 1.25
-        else:
-            position = 1
+            style_factor *= 1.10  # Agressif (+ 10%)
+        elif position == "hijack":
+            style_factor *= 1.05  # Légèrement agressif (+ 5%)
+        elif position == "utg":
+            style_factor *= 0.90  # Très passif hors position (- 10%)
+        elif position == "small_blind":
+            style_factor *= 0.95  # Passif (- 5%)
+        else: # "big_blind" (neutre)
+            style_factor *= 1.00
+        
+        desired_bet_amount = optimal_bet_amount * style_factor
+        
+        if desired_bet_amount > self.stack: #le joueur ne parie pas ce qu'il n'a pas
+            desired_bet_amount = self.stack
 
-        bot_bet = (self.stack_percent(self.win_chance) * self.stack)*position
-        if amount_to_call == 0 and self.stack_percent(self.win_chance) < 0.1:
+        if amount_to_call == 0 and optimal_choice == "check":
             return {"check": True}
-        
-        elif amount_to_call <= bot_bet >= amount_to_call * 1.1:
+
+        elif desired_bet_amount <= amount_to_call <= optimal_bet_amount or optimal_bet_amount <= amount_to_call <= desired_bet_amount:
+            self.stack -= amount_to_call
             return {"call": amount_to_call}
+            
+        elif amount_to_call < desired_bet_amount and amount_to_call < optimal_bet_amount:
+            self.stack -= desired_bet_amount
+            return {"bet": round(desired_bet_amount,0)}
 
-        elif amount_to_call < bot_bet:
-            return {"bet": round(bot_bet,0)}
-
-        elif amount_to_call > bot_bet:
+        else:
             return {"fold": True}
-        
-        
-
-
-
-
-#objet = Tag(win_chance=0.5, position = False, stack=100)
-#print(objet.stack_percent(objet.win_chance))

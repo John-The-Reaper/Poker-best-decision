7723a4ee6f950f72d16e455b1669a5d0aea68ba7
Pr1me000 <overall-rug-dust@duck.com>
2025-11-08 11:37:20 +0000

    Update action method with position multipliers
    Refactor action method to include position-based betting adjustments.

--- players/lag.py
+++ players/lag.py
@@ -18,8 +18,23 @@ class Lag():
         result = self.min_bet + (self.max_bet - self.min_bet) / (1 + np.exp(exponent_input))
         return round(result,2) #renvoie 2 chiffres après la virgule
     
-    def action(self, amount_to_call, board, state):
-        bot_bet = self.stack_percent(self.win_chance) * self.stack
+    def action(self, amount_to_call, position):
+        if position == "utg":
+             position = 1.15
+        elif position == "hijack":
+             position = 1.18
+        elif position == "button":
+            position = 1.45
+        elif position == "small_blind":
+            position = 1.35
+        elif position == "big_blind":
+            position = 1.00
+        elif position == "cutt_off":
+            position = 1.25
+        else:
+            position = 1
+
+        bot_bet = (self.stack_percent(self.win_chance) * self.stack)*position
 
         if amount_to_call == 0 and self.stack_percent(self.win_chance) < 0.1:
             return {"check": True}

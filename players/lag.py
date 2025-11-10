6cf708349197ff74cd37300430c72e6294628c8b
Pr1me000 <overall-rug-dust@duck.com>
2025-11-10 10:17:41 +0000

    Refactor action method to simplify betting logic
    Removed complex betting logic based on player position and added a print statement for debugging.

--- players/lag.py
+++ players/lag.py
@@ -18,36 +18,4 @@ class Lag():
 
     
     def action(self, amount_to_call, position, optimal_bet_amount, optimal_choice):
-        style_factor = self.multiplicator(self.win_chance) 
-        #Avantage position impactant style_factor :
-        if position == "button":
-            style_factor *= 1.15  # Très agressif en position (+ 15%)
-        elif position == "cutt_off":
-            style_factor *= 1.10  # Agressif (+ 10%)
-        elif position == "hijack":
-            style_factor *= 1.05  # Légèrement agressif (+ 5%)
-        elif position == "utg":
-            style_factor *= 0.90  # Très passif hors position (- 10%)
-        elif position == "small_blind":
-            style_factor *= 0.95  # Passif (- 5%)
-        else: # "big_blind" (neutre)
-            style_factor *= 1.00
-        
-        desired_bet_amount = optimal_bet_amount * style_factor
-        
-        if desired_bet_amount > self.stack: #le joueur ne parie pas ce qu'il n'a pas
-            desired_bet_amount = self.stack
-
-        if amount_to_call == 0 and optimal_choice == "check":
-            return {"check": True}
-
-        elif desired_bet_amount <= amount_to_call <= optimal_bet_amount or optimal_bet_amount <= amount_to_call <= desired_bet_amount:
-            self.stack -= amount_to_call
-            return {"call": amount_to_call}
-            
-        elif amount_to_call < desired_bet_amount and amount_to_call < optimal_bet_amount:
-            self.stack -= desired_bet_amount
-            return {"bet": round(desired_bet_amount,0)}
-
-        else:
-            return {"fold": True}
+        return print(amount_to_call, position, optimal_bet_amount, optimal_choice)
